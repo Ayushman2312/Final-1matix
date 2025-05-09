ffa47cce8ba4df62145b00d5c84c3977bd6b2a08
Ayushman2312 <ayushmantiwari2312@gmail.com>
2025-05-09 16:40:23 +0530

    debug = True changed

--- matrix/settings.py
+++ matrix/settings.py
@@ -23,7 +23,7 @@ load_dotenv(env_path)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["89.116.20.128","1matrix.io","www.1matrix.io"]
